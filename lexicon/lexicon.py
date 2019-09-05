c88defb5351cd08a3429a466cb3c351ed5073510
Unknown <stafford.carh@gmail.com>
2019-09-05 13:17:14 -0400

    tests connects
    lexicon_tests actually runs now! Just gotta make lexicon classes to make it pass tests

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -2,4 +2,13 @@ stuff = input('> ')
 words = stuff.split()
 
 
-direction_words = ()
+def scan(words):
+    direction_north = ('direction', 'north')
+    direction_east = ('direction', 'east')
+    direction_west = ('direction', 'west')
+    direction_south = ('direction', 'south')
+
+    # def go_direction():
+    # once the direction is recieved we can have them go to the room in that direction
+
+    # def stop
--- tests/lexicon_tests.py
+++ tests/lexicon_tests.py
@@ -1,2 +1,9 @@
 from nose.tools import *
 from lexicon import lexicon
+
+
+def test_directions():
+    assert_equal(lexicon.scan("north"), [('direction', 'north')])
+    result = lexicon.scan("north south east")
+    assert_equal(result, [('direction', 'north'),
+                          ('direction', 'south'), ('direction', 'east')])
