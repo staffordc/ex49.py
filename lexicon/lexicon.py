b5d4825d66d7d19609d08a3aecfb58df36edbd58
Unknown <stafford.carh@gmail.com>
2019-09-05 21:05:24 -0400

    Passed init Tests
    passed all directions, working on verbs now

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,12 +1,17 @@
-stuff = input('> ')
-words = stuff.split()
+# stuff = input('> ')
+# words = stuff.split()
 
 
 def scan(words):
+    results = []
     direction_north = ('direction', 'north')
     direction_east = ('direction', 'east')
     direction_west = ('direction', 'west')
     direction_south = ('direction', 'south')
+    splitwords = words.split()
+    for word in splitwords:
+        results.append(('direction', word))
+    return results
 
     # def go_direction():
     # once the direction is recieved we can have them go to the room in that direction
--- tests/lexicon_tests.py
+++ tests/lexicon_tests.py
@@ -2,8 +2,19 @@ from nose.tools import *
 from lexicon import lexicon
 
 
+def test_empty_returns_nothing():
+    assert_equal(lexicon.scan(""), [])
+
+
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
+
+
+def test_directions_2():
     result = lexicon.scan("north south east")
     assert_equal(result, [('direction', 'north'),
                           ('direction', 'south'), ('direction', 'east')])
+
+
+def test_verbs():
+    assert_equal(lexicon.scan("go"), [('verb', 'go')])
